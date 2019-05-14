3a88de4e4d0567ef3c6fa40971bda55b4b53dd2b
Justine Berretta <justberretta@gmail.com>
2019-05-14 20:38:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ def introduction(name)
 end
 
 def introduction_with_language(language)
-  puts "Hi, my name is #{name} and I am learning ot pgoram in #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
